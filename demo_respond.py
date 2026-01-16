33ec4fb7dd40a4cf97bec4266d4743c728000460
Silva Joao <jl95terceira@gmail.com>
2026-01-16 10:31:04 +0000

    hello

---
+++ demo_respond.py
@@ -0,0 +1,21 @@
+import socket
+
+from project.package import *
+from project.package import util
+
+server = socket.socket()
+server.bind(("127.0.0.1", 4243,))
+print("Bound")
+server.listen(1)
+sock, addr = server.accept()
+print("Accepted")
+server.close()
+responder = util.responder_from_socket(sock).adapted(bytes.decode, str.encode)
+def respond(msg:str):
+    print(f"<<< {msg}")
+    rmsg = f"{repr(msg)} has {len(msg)} characters"
+    print(f">>> {rmsg}")
+    return rmsg
+responder.respond_forever(respond)
+sock.close()
+print("Done.")
